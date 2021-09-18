2e13c9203071b07a488eef91248ba43f7768af2c
GYashvant <gyashvantkumar@gmail.com>
2021-09-18 20:25:00 +0530

    changes done

---
+++ scripts/payment.js
@@ -0,0 +1,10 @@
+let btnforpay =document.getElementById("paymnetdon")
+
+let details = document.getElementById("paymntsdiv")
+
+ function payclick(){
+    setTimeout (() =>{
+        window.location.href = "paid.html"
+    },2000) 
+
+ }
